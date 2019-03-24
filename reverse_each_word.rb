81e0eb13503a8bc2387973c713a0a904b04889be
Kane Koo <kanekoo@gmail.com>
2019-03-24 01:25:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word (sent)
   sent_array = sent.split(' ')
-  sent_array.each { |word| word.reverse!}
+  sent_array.collect { |word| word.reverse!}
   sent = sent_array.join(' ')
 end
